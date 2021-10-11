67345b52506894234e15b18d5bf265016effd9f2
Renan Souza <renantrsouza@gmail.com>
2021-10-11 13:22:18 -0400

    create button toggle effect

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -2,5 +2,6 @@ const navMenu = document.querySelector(".menu");
 const menuToggle = document.querySelector(".menu-toggle");
 
 menuToggle.addEventListener('click', ()=>{
+    menuToggle.classList.toggle('on')
     navMenu.classList.toggle('active');
 });
