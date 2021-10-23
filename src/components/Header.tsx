8df64779b437b83de35562cf9488252a1dd61cb8
Renan Souza <renantrsouza@gmail.com>
2021-10-23 18:50:46 -0400

    Create Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,24 @@
+export function Header(){
+    return (
+        <>
+        <header>
+                <h2>DevChallenge</h2>
+                <section className="menu-toggle">
+                    <div>
+                        <span className="one"></span>
+                        <span className="two"></span>
+                        <span className="three"></span>
+                        <span className="four"></span>
+                    </div>
+                    <nav>
+                        <ul>
+                            <li><a href="#test">Home</a></li>
+                            <li><a href="#test">Features</a></li>
+                            <li><a href="#test">Contact</a></li>
+                        </ul>
+                    </nav>
+                </section>
+            </header>
+        </>
+    );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,10 @@
+import { Header } from "../components/Header";
 import team from "../assets/images/team.svg";
 
 export function Home(){
     return (
         <>
-            <header>
-                <h2>DevChallenge</h2>
-                <section className="menu-toggle">
-                    <div>
-                        <span className="one"></span>
-                        <span className="two"></span>
-                        <span className="three"></span>
-                        <span className="four"></span>
-                    </div>
-                    <nav>
-                        <ul>
-                            <li><a href="#test">Home</a></li>
-                            <li><a href="#test">Features</a></li>
-                            <li><a href="#test">Contact</a></li>
-                        </ul>
-                    </nav>
-                </section>
-            </header>
+            <Header />
             <main>
                 <img src={team} alt="" />
                 <section>
