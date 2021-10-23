228b73c80d982ea71ff4e38d548bfc12f2ea1df5
Renan Souza <renantrsouza@gmail.com>
2021-10-23 11:55:26 -0400

    Create Home component

---
+++ src/pages/Home.tsx
@@ -0,0 +1,35 @@
+import team from "../assets/images/team.png";
+
+export function Home(){
+    return (
+        <>
+            <header>
+                <h2>DevChallenge</h2>
+                <section className="menu-toggle">
+                    <div>
+                        <span className="one"></span>
+                        <span className="two"></span>
+                        <span className="three"></span>
+                        <span className="four"></span>
+                    </div>
+                    <nav>
+                        <ul>
+                            <li><a href="#test">Home</a></li>
+                            <li><a href="#test">Features</a></li>
+                            <li><a href="#test">Contact</a></li>
+                        </ul>
+                    </nav>
+                </section>
+            </header>
+            <main>
+                <img src={team} alt="" />
+                <section>
+                    <strong>Create amazing graphics</strong>
+                    <p>Colaborate and create beautiful graphics with your team.</p>
+                </section>
+                <a href="https://www.google.com.br" target="_blank" rel="noreferrer">Start now</a>
+                
+            </main>
+        </>
+    );
+}
